f861bac1b8b855aa9c04f1a168b4c8e3de7fdea5
ALEKSANDR399 <88842048+ALEKSANDR399@users.noreply.github.com>
2022-01-21 21:39:15 +0200

    ДЗ№7

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryBoxForCardImage = document.querySelector('.gallery');
+const cardCollection = addCardWithImg(galleryItems);
+
+galleryBoxForCardImage.addEventListener('click', addOpenOriginalSizeImage)
+galleryBoxForCardImage.addEventListener('keydown', closeModalWindow);
+galleryBoxForCardImage.innerHTML = cardCollection;
+
+function addCardWithImg (cards) { 
+    return cards.map(({original, preview, description }) => `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`).join('');
+};
+
+function addOpenOriginalSizeImage(event) {
+  event.preventDefault()
+  if (!event.target.classList.contains("gallery__image")) return;
+
+	basicLightbox.create(`
+		<img width="1280" height="854" src= '${event.target.dataset.source}'>
+	`).show()
+  
+};
+
+console.log(basicLightbox);
+function closeModalWindow(event) {
+  
+  if (basicLightbox) { 
+    if (event.key === "Escape") {
+      basicLightbox.remove();
+    };
+  };
+    
+};
+
+
+
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const boxForGalleryImage = document.querySelector('.gallery');
+const collectionWithImage = createImage(galleryItems);
+
+boxForGalleryImage.innerHTML = collectionWithImage;
+
+var lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt',
+    captionDelay:  '250',
+ });
+
+function createImage (elem) {
+    return elem.map(({preview, original, description}) => `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`).join('');
+};
+
+
+
